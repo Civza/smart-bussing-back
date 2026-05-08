3161df564b687315f4fb180a160b089bd974c09e
Emiliano0X1 <egp2506@gmail.com>
2026-05-07 20:11:28 -0700

    fix minimun error

--- src/main/java/buss/smartbussingapi/Parada/Parada.java
+++ src/main/java/buss/smartbussingapi/Parada/Parada.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Accessors(fluent = false)
 @Entity
 @Table
 public class Parada {
